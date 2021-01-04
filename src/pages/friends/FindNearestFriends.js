7d92201b8277181c56617aa2cb67fdbc5c6a47d7
nayyaung9 <naygit.developer@gmail.com>
2021-01-04 22:52:19 +0630

    feat: develop location access module

---
+++ src/pages/friends/FindNearestFriends.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+const FindNearestFriends = () => {
+  return <Layout>FindNearestFriends</Layout>;
+};
+
+export default FindNearestFriends;
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { makeStyles, Typography } from "@material-ui/core";
-import { useSelector } from "react-redux";
+import { makeStyles, Typography, Container, Switch } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { geolocationActions } from "../../store/actions/geolocation.action";
 
 const useStyles = makeStyles((theme) => ({
   profileSrc: {
     width: theme.spacing(12),
     height: theme.spacing(12),
     textAlign: "center",
+  },
+  rootRow: {
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 }));
 
@@ -16,8 +23,35 @@ const mobileTabActive = {
 };
 
 const Profile = () => {
+  const dispatch = useDispatch();
+
   const classes = useStyles();
   const user = useSelector((state) => state.auth.user);
+
+  const [state, setState] = React.useState({
+    location: false,
+  });
+
+  const handleChange = () => {
+    const error = () => {
+      setState({ ...state, location: false });
+    };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords;
+        const payload = {
+          userId: user._id ? user._id : user.id,
+          geolocation: { latitude, longitude },
+        };
+        dispatch(geolocationActions.getCurrentPosition(payload));
+
+        setState({ ...state, location: true });
+
+        return { latitude, longitude };
+      }, error);
+    }
+  };
+
   return (
     <Layout {...mobileTabActive}>
       <div
@@ -31,6 +65,19 @@ const Profile = () => {
           {user?.fullname}
         </Typography>
       </div>
+      <Container>
+        <div className={classes.rootRow}>
+          <Typography variant="body1" style={{ fontWeight: 800, color: "#333" }}>
+            Location
+          </Typography>
+          <Switch
+            name="location"
+            checked={state.location}
+            onChange={handleChange}
+            inputProps={{ "aria-label": "secondary checkbox" }}
+          />
+        </div>
+      </Container>
     </Layout>
   );
 };
