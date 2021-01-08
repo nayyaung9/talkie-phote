4abc80b2d8a2ed1dc7c859cf3772f5cce3348fe3
nayyaung9 <naygit.developer@gmail.com>
2021-01-08 14:06:56 +0630

    feat: add spinner and change btn style

--- src/store/actions/geolocation.action.js
+++ src/store/actions/geolocation.action.js
@@ -1,5 +1,5 @@
 import api from "../../api";
-import { FETCH_NEARBY_FRIENDS_LIST } from "../actionTypes";
+import { FETCH_NEARBY_FRIENDS_LIST, FETCH_NEARBY_FRIENDS_LIST_REQUEST } from "../actionTypes";
 
 const getCurrentPosition = (payload) => async (dispatch) => {
   const { userId } = payload;
@@ -9,15 +9,24 @@ const getCurrentPosition = (payload) => async (dispatch) => {
 };
 
 const findNearByFrineds = (payload) => async (dispatch, getState) => {
+  const request = (payload) => dispatch({ type: FETCH_NEARBY_FRIENDS_LIST_REQUEST, payload });
+
   const { userId } = payload;
   const { auth } = getState();
   const authUserId = auth.user._id;
 
-  await api.get(`/api/user/${userId}/nearby/friends`, payload).then((res) => {
-    const { data } = res.data;
-    const results = data && data.filter((user) => user._id != authUserId);
-    dispatch({ type: FETCH_NEARBY_FRIENDS_LIST, payload: results });
-  });
+  dispatch(request(true));
+  await api
+    .get(`/api/user/${userId}/nearby/friends`, payload)
+    .then((res) => {
+      const { data } = res.data;
+      dispatch(request(false));
+      const results = data && data.filter((user) => user._id != authUserId);
+      dispatch({ type: FETCH_NEARBY_FRIENDS_LIST, payload: results });
+    })
+    .catch(() => {
+      dispatch(request(false));
+    });
 };
 
 export const geolocationActions = {
